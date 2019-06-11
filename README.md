eb9e201e808f3baac3882c506923c9738fc4746a
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2019-06-11 19:30:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Caesar Cipher
+
+Jun 11 2019
+
+## Description
+
+- This Python program encrypts and decrypts messages based on the [Caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher).
+- The Caesar cipher is a type of substitution cipher where letters are replaced with other letters from a shifted position in an alphabet set.
+  - For example, the letter `'a'`, shifted by two (2) positions, would result in the letter `'c'`.
+
+---
+
+## `cipherMessage(message, key, mode)`
+
+- To encrypt and decrypt messages, use the `cipherMessage(message, key, mode)` function.
+- The `cipherMessage(message, key, mode)` accepts three parameters:
+  - `message` (type: string)
+    - The message parameter accepts any string object.
+    - Enter the message you want encrypt or decrypt in this first parameter.
+  - `key` (type: integer)
+    - Select and enter your desired encryption/decryption key value.
+  - `mode` (type: string)
+    - There are only two (2) modes:
+      - Enter `'e'` if you want to encrypt messages.
+      - Enter `'d'` if you want to decrypt messages.
+
+### Encryption Example
+
+```python
+cipherMessage('Today is beautiful!', 37, 'e')
+
+# 3_~0BD%;D-!0.,%@.*S
+```
+
+- Remember to enter `'e'` as the mode parameter so that the `cipherMessage()` function is set properly to encrypt messages.
+- When you encrypt the message string `'Today is beautiful!'`, the `cipherMessage()` function will return the encrypted string message: `'3_~0BD%;D-!0.,%@.*S'`.
+
+### Decryption Example
+
+```python
+cipherMessage('3_~0BD%;D-!0.,%@.*S', 37, 'd')
+
+# Today is beautiful!
+```
+
+- Remember to enter `'d'` as the mode parameter so that the `cipherMessage()` function is set properly to decrypt messages.
+- When you decrypt the encrypted message string `'3_~0BD%;D-!0.,%@.*S'`, the `cipherMessage()` function will return the decrypted string message: `'Today is beautiful!'`.
+
+---
+
+## Challenges
+
+### Decryption With Key
+
+- Decrypt the following:
+    > O zs2m45xm4u z3?-k 5-tm6q-35ooq33r5xx9-pqo29`4qp-4tu3-yq33msq?
+    >
+    > *(Hint: 12)*
+
+### Decryption Without Key
+
+- Decrypt the following:
+    > u9@*w= *w7w&381A
+
+- Explore the handy `magicOutput(message)` function!
+
+---
+
+## Future Development
+
+- [ ] TODO: Dictionary lookup for `magicOutput()` function. Return corresponding keys in descending order of relevance.
+  - [ ] <https://pypi.org/project/PyDictionary/>
+  - [ ] <https://pypi.org/project/pyenchant/#description>
+- [ ] TODO: Links conversion `/`.
+- [ ] TODO: Error handling for edge cases.
+- [ ] TODO: Front-end GUI.
+
+---
+
+## Resources
+
+- <https://en.wikipedia.org/wiki/Caesar_cipher>
+- <https://inventwithpython.com/cracking/chapter5.html>
---
+++ caesarCipher.py
@@ -0,0 +1,77 @@
+# ================================================================
+# caesarCipher.py
+# TODO: Compare output to an English dictionary and find which keyTest is the most likely message.
+# TODO: Links conversion. Forward-slash '/' correct conversion
+
+# ================================================================
+# Initial variables
+
+import os
+
+symbolString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz `1234567890-=~!@#$%^&*()_+?:;,.<>\/\\'
+# lengthOfSymbolString = len(symbolString) # = 86
+
+personalMessage = 'Hello my friends!'
+key = 70
+mode = 'e'
+
+# ================================================================
+# cipherMessage Encryption/Decryption Function
+def cipherMessage(message, key, mode):
+
+    # Growing output string
+    outputString = ''
+
+    # Convert message to symbolIndex
+    for character in message:
+    
+        lengthSymbolString = len(symbolString)
+
+        symbolIndex = symbolString.find(character)
+
+        # Handle mode; encrypt or decrypt
+        if mode == 'e':
+            encodedIndex = symbolIndex + key
+        elif mode == 'd':
+            encodedIndex = symbolIndex - key
+
+        # Create outputString
+        # Wrap around when encodedIndex is greater than the length of the symbolString
+        if encodedIndex > len(symbolString) - 1:
+            outputString = outputString + symbolString[encodedIndex % lengthSymbolString]
+        
+        # Wrap around when encodedIndex is less than zero (negative)
+        elif encodedIndex < 0:
+            localIndex = len(symbolString) - (encodedIndex % lengthSymbolString)
+            outputString = outputString + symbolString[lengthSymbolString - localIndex]
+        
+        # When encodedIndex within the symbolString
+        else:
+            outputString = outputString + symbolString[encodedIndex]
+    
+    print(outputString)
+    return outputString
+
+# ================================================================
+# Magic Decryption
+
+# Output location of the magicDecryption() function
+desktopPath = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
+# desktopPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+
+def magicDecryption(message):
+    
+    print ('messagesOutput.txt location:\n' + desktopPath)
+    
+    # Create/overwrite messagesOutput.txt document.
+    messagesOutput = open(desktopPath + '\\messagesOutput.txt', 'w')
+    messagesOutput.write('Keys:'  + '\n')
+    messagesOutput.write('====================================='  + '\n')
+    messagesOutput.close()
+
+    # Loop through every key combination and write it to file.
+    for keyTest in range(86):
+        keyTestMessage = cipherMessage(message, keyTest, 'd')
+        messagesOutput = open(desktopPath + '\\messagesOutput.txt', 'a')
+        messagesOutput.write(str(keyTest) + ': '+ keyTestMessage + '\n')
+        messagesOutput.close()
